7f042b333db4a10054710d4a4e20984f63bafd58
Alejandro <alejandrofulljuegos@gmail.com>
2021-11-30 14:17:49 -0500

    primeros cambios

--- app/src/main/java/com/example/interfazpractica/MainActivity.java
+++ app/src/main/java/com/example/interfazpractica/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.interfazpractica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnEnviar(View view){
+        Intent intent = new Intent (MainActivity.this,MainActivity2.class);
+
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        EditText txtFecha = (EditText)findViewById(R.id.txtFecha);
+        EditText txtTelefono = (EditText)findViewById(R.id.txtTelefono);
+
+
+        Bundle b = new Bundle();
+
+        b.putString("NOMBRE", txtNombre.getText().toString() + " \n Nacido: " + txtFecha.getText().toString() +"\n Telefono: " + txtTelefono.getText().toString());
+
+        intent.putExtras(b);
+
+        startActivity(intent);
+
+
     }
 }
